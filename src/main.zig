3bf2be12b604b5cf508256724e5444522d84bcf9
daniel.forde <daniel.forde@lifeq.com>
2022-06-24 13:25:27 +0200

    Complete first part of ray tracing in one weekend.

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,29 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    var alloc = std.heap.GeneralPurposeAllocator(.{}){};
+    const image_width = 256;
+    const image_height = 256;
 
-    var list = std.ArrayList(u8).init(alloc.allocator());
+    var file = try std.fs.cwd().createFile("img.ppm", .{});
+    defer file.close();
 
-    std.debug.print("list = {any}\n", .{list.items});
+    var writer = file.writer();
 
-    try list.append(1);
+    try std.fmt.format(writer, "P3\n{} {}\n255\n", .{ image_width, image_height });
 
-    std.debug.print("list = {any}\n", .{list.items});
+    var j: i32 = image_height - 1;
+    while (j >= 0) : (j -= 1) {
+        var i: i32 = 0;
+        while (i != image_width) : (i += 1) {
+            const r = @intToFloat(f32, i) / @intToFloat(f32, image_width - 1);
+            const g = @intToFloat(f32, j) / @intToFloat(f32, image_height - 1);
+            const b: f32 = 0.25;
 
-    _ = list;
+            const ir = @floatToInt(i32, 255.999 * r);
+            const ig = @floatToInt(i32, 255.999 * g);
+            const ib = @floatToInt(i32, 255.999 * b);
+
+            try std.fmt.format(writer, "{} {} {}\n", .{ ir, ig, ib });
+        }
+    }
 }
