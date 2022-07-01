3c354bc8eaa073339c264764d01c0699480d2718
daniel.forde <daniel.forde@lifeq.com>
2022-07-01 09:20:07 +0200

    Complete 9.6.

--- src/metal.zig
+++ src/metal.zig
@@ -5,10 +5,11 @@ const HitRecord = @import("hittable.zig").HitRecord;
 
 pub const Metal = struct {
     albedo: Colour,
+    fuzz: f32,
 
     pub fn scatter(self: *const Metal, r: *const Ray, rec: *HitRecord, att: *Colour, s: *Ray) bool {
         const ref = Vec.reflect(&r.dir.unit(), &rec.n);
-        s.* = Ray{ .orig = rec.p, .dir = ref };
+        s.* = Ray{ .orig = rec.p, .dir = Vec.addv(&[_]Vec{ ref, Vec.randUnitSphere().mulf(self.fuzz) }) };
         att.* = self.albedo;
         return (s.dir.dot(&rec.n) > 0.0);
     }
